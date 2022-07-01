eb5cfcb1057cbfb3d4f0095b9f2305c5b822c3f4
LOKESH KUMAR JHA <83881305+lokesh1jha@users.noreply.github.com>
2022-07-02 00:49:46 +0530

    class is created with different methods

--- app.js
+++ app.js
@@ -12,16 +12,12 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactUs = require('./routes/contactUs');
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use(contactUs);
 app.use(shopRoutes);
-
 
 app.use(errorController.get404);
 
